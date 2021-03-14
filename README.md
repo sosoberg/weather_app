0a023319705e69d180d57bec5378ce7405f9f894
Sam Oberg <samueloberg@hotmail.com>
2021-03-14 14:19:49 -0700

    present data data working with js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # weather_app
+
+## Description
+
+This is a basic weather app that will give you the tempature, humidity, wind speeds and UV of any United States city. It will also give a 5 day forecast the Open Weather Map API
+
+## Usage
+
+<!-- screenshot of webpage -->
+
+Link to deployed webpage
+<!-- link to webpage -->
+
+## Sources
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,52 @@
-const weatherAPI = fetch("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8&q")
+// weather object with fetche call
+let weather = {
+    fetchWeather: function (city) {
+        fetch (
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=b8b1e554d2f82db6bf0383dbd3f679e8'
+        ).then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
 
-console.log(weatherAPI)
-/*
-var searchBar = document.getElementById('searchBar');
-var cityName = document.querySelector('#cityName')
-var tempature = document.querySelector('#tempature');
-searchBar.addEventListener('keyup', (e) => {
-    console.log(e.target.value);
-    city = e.target.value;
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=b8b1e554d2f82db6bf0383dbd3f679e8')
-    .then(result => result.json()) 
-    .then(data => console.log(data))
-    cityName.innerHTML = city;
-    tempature.innerHTML = data.main.temp;
-})
-*/
+        // finding the data in the fetch object
+        const { name } = data;
 
-console.log(window)
+        // as an array in the object, must use [0]
+        const { icon } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, temp, humidity, speed);
 
-function getParams() {
-    var searchParamsArr = document.location.search
-    console.log(searchParamsArr);
-
-    var query = searchParamsArr[0].split('=').pop();
-
-    searchAPI(query)
-}
-
-var titleEl = document.querySelector('#cityName');
-
-var resultCard = document.querySelector('#card')
-function printResults(resultObj) {
-    console.log(resultObj);
-
-    
-    titleEl.textContent = resultObj.data.name;
-
-    var tempContent = document.querySelector('#tempature')
-    tempContent.innerHTML = '<strong>Tempature:</strong> ' + resultObj.data.main.temp;
-
-    var humidContent = document.querySelector('#humidity');
-    humidContent.innerHTML = '<strong>Humidity:</strong> ' + resultObj.data.main.humidity;
-
-    var windContent = document.querySelector('#windSpeed');
-    windContent.innerHTML = '<strong>Wind Speed:</strong> ' + resultObj.data.wind.speed;
-
-    resultCard.append(titleEl, tempContent, humidContent, windContent);
-}
-
-function searchAPI(query) {
-    var locQueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8';
-
-    locQueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ query + '&appid=b8b1e554d2f82db6bf0383dbd3f679e8';
-
-    fetch(locQueryUrl).then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-
-        return response.json();  
-    })
-    .then(function (locRes) {
-        titleEl.append = locRes.data.name;
-
-        console.log(locRes);
-        for (var i = 0; i < locRes.results.length; i++) {
-            printResults(locRes.results[i]);
-        }
-    })
-
+        // call data and place in the HTML
+        document.querySelector('#cityName').innerHTML = name;
+        //document.querySelector('#icon').src = 'http://openweathermap.org/img/wn/10d@2x.png';
+        document.querySelector('#humidity').innerHTML = humidity;
+        document.querySelector('#windSpeed').innerHTML = speed;
+        document.querySelector('#tempature').innerHTML = temp;
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector('#searchBar').value);
+    }
 };
 
-var searchBar = document.querySelector('#searchBar').value;
+document.querySelector('.searchBtn').addEventListener('click', function () {
+    weather.search();
+});
 
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
 
-    searchAPI(searchBar);
-}
 
-var searchBtn = document.querySelector('.searchBtn');
 
-searchBtn.addEventListener('click', handleSearchFormSubmit);
 
-getParams();
+
+
+
+
+
+
+
+
+
+
+
+
 
