9cfee449c3b115bc8bbb22063539c988f32c511e
Sam Oberg <samueloberg@hotmail.com>
2021-03-14 13:11:35 -0700

    more JS worked on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const weatherAPI = fetch("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8&q")
 
 console.log(weatherAPI)
-
+/*
 var searchBar = document.getElementById('searchBar');
 var cityName = document.querySelector('#cityName')
 var tempature = document.querySelector('#tempature');
@@ -14,7 +14,7 @@ searchBar.addEventListener('keyup', (e) => {
     cityName.innerHTML = city;
     tempature.innerHTML = data.main.temp;
 })
-
+*/
 
 console.log(window)
 
@@ -27,24 +27,22 @@ function getParams() {
     searchAPI(query)
 }
 
+var titleEl = document.querySelector('#cityName');
+
 var resultCard = document.querySelector('#card')
 function printResults(resultObj) {
     console.log(resultObj);
 
-    var titleEl = document.createElement('h2');
-    titleEl.id('cityName');
+    
     titleEl.textContent = resultObj.data.name;
 
-    var tempContent = document.textContent.createElement('p');
-    tempContent.id('tempature');
+    var tempContent = document.querySelector('#tempature')
     tempContent.innerHTML = '<strong>Tempature:</strong> ' + resultObj.data.main.temp;
 
-    var humidContent = document.textContent.createElement('p');
-    humidContent.id('humidity');
+    var humidContent = document.querySelector('#humidity');
     humidContent.innerHTML = '<strong>Humidity:</strong> ' + resultObj.data.main.humidity;
 
-    var windContent = document.textContent.createElement('p');
-    windContent.id('windSpeed');
+    var windContent = document.querySelector('#windSpeed');
     windContent.innerHTML = '<strong>Wind Speed:</strong> ' + resultObj.data.wind.speed;
 
     resultCard.append(titleEl, tempContent, humidContent, windContent);
@@ -66,9 +64,24 @@ function searchAPI(query) {
         titleEl.append = locRes.data.name;
 
         console.log(locRes);
-
+        for (var i = 0; i < locRes.results.length; i++) {
+            printResults(locRes.results[i]);
+        }
     })
 
 };
 
-console.log(searchAPI('temp', 'Houston'));
+var searchBar = document.querySelector('#searchBar').value;
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    searchAPI(searchBar);
+}
+
+var searchBtn = document.querySelector('.searchBtn');
+
+searchBtn.addEventListener('click', handleSearchFormSubmit);
+
+getParams();
+
