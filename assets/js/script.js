196bd7f913ec54733556a6b86244bce8dcbf9da0
Sam Oberg <samueloberg@hotmail.com>
2021-03-14 09:50:29 -0700

    adding js functionality to find weather based on search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,74 @@
-const weatherAPI = fetch("api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8")
+const weatherAPI = fetch("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8&q")
 
-console.log(weatherAPI);
+console.log(weatherAPI)
+
+var searchBar = document.getElementById('searchBar');
+var cityName = document.querySelector('#cityName')
+var tempature = document.querySelector('#tempature');
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+    city = e.target.value;
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=b8b1e554d2f82db6bf0383dbd3f679e8')
+    .then(result => result.json()) 
+    .then(data => console.log(data))
+    cityName.innerHTML = city;
+    tempature.innerHTML = data.main.temp;
+})
+
+
+console.log(window)
+
+function getParams() {
+    var searchParamsArr = document.location.search
+    console.log(searchParamsArr);
+
+    var query = searchParamsArr[0].split('=').pop();
+
+    searchAPI(query)
+}
+
+var resultCard = document.querySelector('#card')
+function printResults(resultObj) {
+    console.log(resultObj);
+
+    var titleEl = document.createElement('h2');
+    titleEl.id('cityName');
+    titleEl.textContent = resultObj.data.name;
+
+    var tempContent = document.textContent.createElement('p');
+    tempContent.id('tempature');
+    tempContent.innerHTML = '<strong>Tempature:</strong> ' + resultObj.data.main.temp;
+
+    var humidContent = document.textContent.createElement('p');
+    humidContent.id('humidity');
+    humidContent.innerHTML = '<strong>Humidity:</strong> ' + resultObj.data.main.humidity;
+
+    var windContent = document.textContent.createElement('p');
+    windContent.id('windSpeed');
+    windContent.innerHTML = '<strong>Wind Speed:</strong> ' + resultObj.data.wind.speed;
+
+    resultCard.append(titleEl, tempContent, humidContent, windContent);
+}
+
+function searchAPI(query) {
+    var locQueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=b8b1e554d2f82db6bf0383dbd3f679e8';
+
+    locQueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ query + '&appid=b8b1e554d2f82db6bf0383dbd3f679e8';
+
+    fetch(locQueryUrl).then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+
+        return response.json();  
+    })
+    .then(function (locRes) {
+        titleEl.append = locRes.data.name;
+
+        console.log(locRes);
+
+    })
+
+};
+
+console.log(searchAPI('temp', 'Houston'));
